d8c0c0d30dfc7b2604ae8b73a022391653f7cdcd
Sesseno <sfsena@gmail.com>
2022-02-01 16:11:47 -0300

    Atualização JSON, português.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { SugestoesComponent } from './sugestoes/sugestoes.component';
-
 import { EspecialMesComponent } from './especial-mes/especial-mes.component';
-
 import { SagaComponent } from './saga/saga.component';
-
 import { DireitoComponent } from './direito/direito.component';
 import { TeatroComponent } from './teatro/teatro.component';
 import { EmpreendedorismoComponent } from './empreendedorismo/empreendedorismo.component';
@@ -52,11 +49,11 @@ const routes: Routes = [
     component: BiblioteconomiaComponent,
     ...canActivate(enviarSemLogin),
   },
-  {
-    path: 'psicologia',
-    component: PsicologiaComponent,
-    ...canActivate(enviarSemLogin),
-  },
+  // {
+  //   path: 'psicologia',
+  //   component: PsicologiaComponent,
+  //   ...canActivate(enviarSemLogin),
+  // },
   {
     path: 'saga',
     component: SagaComponent,
